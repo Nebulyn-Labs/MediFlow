952f47d3ee5a20f7022e49b805b3dc998d7bdf0f
Himanshu Pathak <himanshu2725pathak@gmail.com>
2026-08-02 16:39:49 +0000

    fix(test): use exact package import and class identifier for AI service

--- test/models_and_services_test.dart
+++ test/models_and_services_test.dart
@@ -1,4 +1,4 @@
-import 'package:mediflow/services/ai_service.dart';
+import 'package:med_supply_prototype/services/ai_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:latlong2/latlong.dart';
