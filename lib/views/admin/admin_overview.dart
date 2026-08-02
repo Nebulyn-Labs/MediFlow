4cfb778ea8660429d94cb5c9a656a83c516f9888
kiranShamsHere <kiranshamsdhiloo@gmail.com>
2026-08-03 00:31:00 +0500

    fix(admin): flex KPI and health cards on narrow viewports

--- lib/views/admin/admin_overview.dart
+++ lib/views/admin/admin_overview.dart
@@ -482,7 +482,7 @@ class _AdminOverviewState extends ConsumerState<AdminOverview> {
       onTap: onTap,
       borderRadius: BorderRadius.circular(12),
       child: Container(
-        width: 250,
+        constraints: const BoxConstraints(minWidth: 160, maxWidth: 250),
         padding: const EdgeInsets.all(24),
         decoration: BoxDecoration(
           color: MediColors.surface,
@@ -556,7 +556,7 @@ class _AdminOverviewState extends ConsumerState<AdminOverview> {
     }
 
     return Container(
-      width: 280,
+      constraints: const BoxConstraints(minWidth: 200, maxWidth: 280),
       padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
         color: MediColors.surface,
